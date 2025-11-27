937a53e27848e918358b8ff331daa81b99d96851
maxime <maxime.ducandas@gmail.com>
2025-11-27 10:54:20 +0100

    v0.1.1

---
+++ code/back_end/game.js
@@ -0,0 +1,212 @@
+
+class card {
+    #_type = 0;
+    constructor(type) {
+        this.#_type = type;
+    }
+
+    get type(){
+        return this.#_type;
+    }
+
+
+    activate(player_played,player_attacked){
+        switch (this.#_type) {
+            case 0:
+                player_played.discard(this);
+                player_played.equiped_card.push(this);
+                return true
+            case 1:
+                guess =0;
+                if(player_attacked.currentcard[0].type() == guess){
+                    player_attacked.iseliminated = true
+                    return true
+                }
+                return false
+                
+            case 2:
+                print(player_attacked.currentcard[0].type())
+                
+                break;
+            case 3:
+                if(player_played.currentcard[0].type() > player_attacked.currentcard[0].type()){
+                    player_attacked.iseliminated = true
+                }
+                else{
+                    player_played.iseliminated = true
+                }
+
+                
+                break;
+            case 4:
+                player_played.equiped_card.push(this);
+                
+                break;
+            case 5:
+                player_attacked.discard();
+                player_attacked.draw();
+                
+                break;
+            case 6:
+                player_played.draw();
+                player_played.draw();
+                player_played.putback();
+                player_played.putback();
+                
+                break;
+            case 7:
+                temp = player_attacked.currentcard[0];
+                player_attacked.currentcard[0] = player_played.currentcard[0];
+                player_played.currentcard[0] = temp;
+                
+                break;
+            case 8:
+                
+                break;
+            case 9:
+                player_played.iseliminated = true;
+                
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+
+}
+
+class player {
+    iseliminated = false
+    #current_card = []
+    equiped_card = []
+    deck = null
+    discard_pile = null
+    constructor(deck,discard_pile){
+        this.#current_card = deck.draw();
+        this.deck = deck;
+        this.discard_pile = discard_pile;
+    }
+    get currentcard(){
+        return this.#current_card;
+    }
+    draw(){
+        //draw a card from deck
+        this.#current_card.push(this.deck.draw());
+    }
+    discard(card=null){
+        //discard a card
+        if(card == null){
+            card = this.#current_card[0];
+            this.#current_card.shift();
+
+        }
+        else{
+            let index = this.#current_card.indexOf(card);
+            if(index > -1){
+                this.#current_card.splice(index,1);
+            }
+        }
+        
+    }
+    putback(){
+        //put back a card at end of deck
+        this.deck.putback(this.#current_card[0]);
+    }
+
+}
+
+function print(data){
+    console.log(data)
+}
+
+class deck {
+    #cards = []
+    constructor(){
+        this.#cards.push(new card(0));
+        this.#cards.push(new card(0));
+        this.#cards.push(new card(1));
+        this.#cards.push(new card(1));
+        this.#cards.push(new card(1));
+        this.#cards.push(new card(1));
+        this.#cards.push(new card(1));
+        this.#cards.push(new card(1));
+        this.#cards.push(new card(2));
+        this.#cards.push(new card(2));
+        this.#cards.push(new card(3));
+        this.#cards.push(new card(3));
+        this.#cards.push(new card(4));
+        this.#cards.push(new card(4));
+        this.#cards.push(new card(5));
+        this.#cards.push(new card(5));
+        this.#cards.push(new card(6));
+        this.#cards.push(new card(6));
+        this.#cards.push(new card(7));
+        this.#cards.push(new card(8));
+        this.#cards.push(new card(9));
+        this.#cards = this.#cards.sort(() => Math.random() - 0.5);
+        this.draw();
+    }
+    draw(){
+        return this.#cards.pop();
+    }
+    putback(card){
+        this.#cards.unshift(card);
+    }
+
+}
+
+class game {
+    #players = []
+    #deck = null
+    constructor(num_players){
+        this.#deck = new deck();
+        for(let i=0;i<num_players;i++){
+            this.#players.push(new player(this.#deck));
+        }
+    }
+
+    start(){
+        //game loop
+        while(this.#players.filter(p => !p.iseliminated).length > 1){
+            for(let i=0;i<this.#players.length;i++){
+                if(this.#players[i].iseliminated){
+                    continue;
+                }
+                this.#players[i].draw();
+                //player chooses a card to play
+                let played_card = this.#players[i].currentcard[0];
+                let attacked_player_index = (i+1)%this.#players.length;
+                while(this.#players[attacked_player_index].iseliminated){
+                    attacked_player_index = (attacked_player_index+1)%this.#players.length;
+                }
+                let attacked_player = this.#players[attacked_player_index];
+                played_card.activate(this.#players[i],attacked_player);
+                this.#players[i].discard();
+            }
+        }
+        let winner = this.#players.find(p => !p.iseliminated);
+        print("Player "+this.#players.indexOf(winner)+" wins!");
+
+    }
+
+    restart(){
+        this.#players = []
+        this.#deck = new deck();
+        for(let i=0;i<num_players;i++){
+            this.#players.push(new player(this.#deck));
+        }
+    }
+
+}
+
+class discard_pile {
+    #cards = []
+    constructor(){
+        this.#cards = []
+    }
+    add(card){
+        this.#cards.push(card);
+    }
+}

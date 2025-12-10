2e7ecf5cceddcf99bff85daf7652e74feb7fbca9
J-infantes <julia.infantes@etud.univ-angers.fr>
2025-12-10 16:19:33 +0100

    Update ai.js

--- code/back_end/ai.js
+++ code/back_end/ai.js
@@ -1,176 +1,227 @@
+import { player } from "./game.js";
+
 function getRandomInt(max) {
-    max = Math.floor(max);
-    return Math.floor(Math.random() * max);
-}
-
-cardsnotplayed = [0,0,1,1,1,1,1,1,2,2,3,3,4,4,5,5,6,6,7,8,9]
-function update(cardplayed){
-    //A FINIR
-}
-
-function play(card1, card2){
-    //Princesse
-    if (card1.type() == 9){
-        return card2;
+        max = Math.floor(max);
+        return Math.floor(Math.random() * max);
     }
 
-    //Comtesse
-    if (card1.type() == 8){
-        if (card2.type() == 5 || card2.type() == 7 || card2.type() == 9){
-            return card1;
-        }
-        else{
-            return card2;
-        }
-    }
+class ia extends player {
+    cardsnotplayed = [0,0,1,1,1,1,1,1,2,2,3,3,4,4,5,5,6,6,7,8,9];
 
-    //Roi
-    if (card1.type() == 7){
-        if (card2.type() == 9){
-            return card1;
-        }
-        else{
-            return card2;
-        }
-    }
-
-    //Chancelier
-    if (card1.type() == 6){
-        if (card2.type() == 0 || card2.type() == 3 || card2.type() == 4){
-            return card2;
-        }
-        else{
-            return card1;
-        }
-    }
-
-    //Prince
-    if (card1.type() == 5){
-        if (card2.type() == 7 || card2.type() == 9){
-            return card1;
-        }
-        else{
-            return card2;
-        }
-    }
-
-    //Servante
-    if (card1.type() == 4){
-        return card1;
-    }
-
-    //Baron
-    if (card1.type() == 3){
-        if (card2.type() == 0 || card2.type() == 1 || card2.type() == 2 || card2.type() == 4){
-            return card2;
-        }
-        else{
-            return card1;
-        }
-    }
-
-    //Pretre
-    if (card1.type() == 2){
-        if (card2.type() == 0 || card2.type() == 4 || card2.type() == 6){
-            return card2;
-        }
-        else{
-            return card1;
-        }
-    }
-
-    //Garde
-    if (card1.type() == 1){
-        if (card2.type() == 0 || card2.type() == 2 || card2.type() == 4 || card2.type() == 6){
-            return card2;
-        }
-        else{
-            return card1;
-        }
-    }
-
-    //Espionne
-    if (card1.type() == 0){
-        if (card2.type() == 4){
-            return card2;
-        }
-        else{
-            return card1;
-        }
-    }
-}
-
-//En cas de choix du chancelier, on prend la carte la plus haute ou l'espionne, mais aucune n'a priorité sur l'autre.
-function chancelier(card1, card2, card3){
-    if (card1.type() == 0 || card1.type() >= card2.type() && card1.type() >= card3.type()){
-        return card1;
-    }
-    if (card2.type() == 0 || card2.type() >= card1.type() && card2.type() >= card3.type()){
-        return card2;
-    }
-    if (card3.type() == 0 || card3.type() >= card1.type() && card3.type() >= card2.type()){
-        return card3;
-    }
-}
-
-function attacksimple(joueurs, me){
-    i = getRandomInt(joueurs.length);
-    protec = false;
-    while (joueurs[i] == me || protec == true){
-        protec = false;
-        i = getRandomInt(joueurs.length);
-        for (let i = 0; i < joueurs[i].equiped_card.length; i++){
-            if (joueurs[i].equiped_card.type() == 4){
-                protec = true;
+    update(cardplayed){
+        typeplayed = cardplayed.type();
+        suppressed = false;
+        let i = 0;
+        while (suppressed == false){
+            if (this.cardsnotplayed[i] == typeplayed){
+                this.cardsnotplayed.splice(i, 1);
+                suppressed = true;
             }
         }
     }
-    return joueurs[i];
-}
 
-function guardsimple(){
-    r = 1;
-    while (r == 1){
-        r = getRandomInt(10);
+    play(card1, card2){
+        //Princesse
+        if (card1.type() == 9){
+            return card2;
+        }
+
+        //Comtesse
+        if (card1.type() == 8){
+            if (card2.type() == 5 || card2.type() == 7 || card2.type() == 9){
+                return card1;
+            }
+            else{
+                return card2;
+            }
+        }
+
+        //Roi
+        if (card1.type() == 7){
+            if (card2.type() == 9){
+                return card1;
+            }
+            else{
+                return card2;
+            }
+        }
+
+        //Chancelier
+        if (card1.type() == 6){
+            if (card2.type() == 0 || card2.type() == 3 || card2.type() == 4){
+                return card2;
+            }
+            else{
+                return card1;
+            }
+        }
+
+        //Prince
+        if (card1.type() == 5){
+            if (card2.type() == 7 || card2.type() == 9){
+                return card1;
+            }
+            else{
+                return card2;
+            }
+        }
+
+        //Servante
+        if (card1.type() == 4){
+            return card1;
+        }
+
+        //Baron
+        if (card1.type() == 3){
+            if (card2.type() == 0 || card2.type() == 1 || card2.type() == 2 || card2.type() == 4){
+                return card2;
+            }
+            else{
+                return card1;
+            }
+        }
+
+        //Pretre
+        if (card1.type() == 2){
+            if (card2.type() == 0 || card2.type() == 4 || card2.type() == 6){
+                return card2;
+            }
+            else{
+                return card1;
+            }
+        }
+
+        //Garde
+        if (card1.type() == 1){
+            if (card2.type() == 0 || card2.type() == 2 || card2.type() == 4 || card2.type() == 6){
+                return card2;
+            }
+            else{
+                return card1;
+            }
+        }
+
+        //Espionne
+        if (card1.type() == 0){
+            if (card2.type() == 4){
+                return card2;
+            }
+            else{
+                return card1;
+            }
+        }
     }
-    return r;
+
+    //En cas de choix du chancelier, on prend la carte la plus haute ou l'espionne, mais aucune n'a priorité sur l'autre.
+    chancelier(card1, card2, card3){
+        if (card1.type() == 0 || card1.type() >= card2.type() && card1.type() >= card3.type()){
+            return card1;
+        }
+        if (card2.type() == 0 || card2.type() >= card1.type() && card2.type() >= card3.type()){
+            return card2;
+        }
+        if (card3.type() == 0 || card3.type() >= card1.type() && card3.type() >= card2.type()){
+            return card3;
+        }
+    }
 }
 
-function attackdifficult(joueurs, me){
-    cibles = []
-    for (let j = 0; j < joueurs.length; j++){
-        for (let k = 0; k < joueurs[j].equiped_card.length; k++){
-            if (joueurs[j].equiped_card[k].type() == 0 && joueurs[j] != me){
+class iasimple extends ia {
+
+    attack(joueurs, me){
+        i = 0;
+        protec = true;
+        while (joueurs[i] == me || protec == true){
+            protec = false;
+            i = getRandomInt(joueurs.length);
+            for (let j = 0; j < joueurs[i].equiped_card.length; j++){
+                if (joueurs[i].equiped_card[j].type() == 4){
+                    protec = true;
+                }
+            }
+        }
+        return joueurs[i];
+    }
+
+    guard(){
+        r = 1;
+        while (r == 1){
+            r = getRandomInt(10);
+        }
+        return r;
+    }
+}
+
+class iadifficult extends ia {
+
+    attack(joueurs, me){
+        cibles = [];
+        for (let j = 0; j < joueurs.length; j++){
+            for (let k = 0; k < joueurs[j].equiped_card.length; k++){
+                if (joueurs[j].equiped_card[k].type() == 0 && joueurs[j] != me){
+                    cibles.push(joueurs[j]);
+                }
+            }
+            if (joueurs[j].points != 0 && joueurs[j] != me){
                 cibles.push(joueurs[j]);
             }
         }
-        if (joueurs[j].points != 0 && joueurs[j] != me){
-            cibles.push(joueurs[j]);
-        }
-    }
 
-    if (cibles.length == 0){
-        cibles = joueurs
-    }
 
-    i = getRandomInt(cibles.length);
-    protec = false;
-    while (cibles[i] == me || protec == true){
-        protec = false;
-        i = getRandomInt(cibles.length);
-        for (let l = 0; l < cibles[i].equiped_card.length; l++){
-            if (cibles[i].equiped_card[l].type() == 4){
-                protec = true;
+        if (cibles.length != 0){
+            for (let e = 0; e < cibles.length; e++){
+                servante = false;
+                for (let r = 0; r < joueurs[e].equiped_card.length; r++){
+                    if (joueurs[e].equiped_card[r].type() == 4){
+                        servante = true;
+                    }
+                }
+                if (servante == true){
+                    cibles.splice(e, 1);
+                }
             }
         }
-    }
-    return cibles[i];
-}
+        if (cibles.length == 0){
+            cibles = joueurs;
+        }
 
-function guarddifficult(){
-    r = getRandomInt(cardsnotplayed.length);
-    while (cardsnotplayed[r] == 1){
-        r = getRandomInt(cardsnotplayed.length)
+        i = 0;
+        protec = true;
+        while (cibles[i] == me || protec == true){
+            protec = false;
+            i = getRandomInt(cibles.length);
+            for (let l = 0; l < cibles[i].equiped_card.length; l++){
+                if (cibles[i].equiped_card[l].type() == 4){
+                    protec = true;
+                }
+            }
+        }
+        return cibles[i];
     }
-    return cardsnotplayed[r];
+
+    guard(){
+
+        onlyguards = true;
+
+        for (let i = 0; i < this.cardsnotplayed.length; i++){
+            if (this.cardsnotplayed[i]!=1){
+                onlyguards = false;
+            }
+        }
+
+        if (onlyguards == false){
+            r = getRandomInt(this.cardsnotplayed.length);
+            while (this.cardsnotplayed[r] == 1){
+                r = getRandomInt(this.cardsnotplayed.length)
+            }
+            return this.cardsnotplayed[r];
+        }
+        else {
+            r = 1;
+            while (r == 1){
+                r = getRandomInt(10);
+            }
+            return r;
+        }
+    }
 }
